53b6ed06832c2e34cfd8b22cef1ceb31bbf36fd1
zakerous <m.z.humayoon@gmail.com>
2024-11-26 20:00:45 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,77 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+import no0Projects80 from "./helka/0-projects.svg";
+import no8Projects80 from "./helka/8-projects.svg";
+import constructionIllustration from "./helka/construction-vector-projects-80.svg";
+import footerIllustrations from "./helka/footer-illustrations.png";
+import helkaFooterLogo from "./helka/helka-footer-logo.svg";
+import helkaHeaderLogo from "./helka/helka-header-logo.svg";
+import heroBg from "./helka/hero-bg.png";
+import heroTitle from "./helka/hero-title.svg";
+import heroSubtitle from "./helka/hero-subtitle.svg";
+import istiraklerSectionContainers from "./helka/istirakler-section-containers.svg";
+import mapIcon from "./helka/map-icon.png";
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+  no0Projects80,
+  no8Projects80,
+  constructionIllustration,
+  footerIllustrations,
+  helkaFooterLogo,
+  helkaHeaderLogo,
+  heroBg,
+  heroTitle,
+  heroSubtitle,
+  istiraklerSectionContainers,
+  mapIcon,
+  // Add more SVGs here if needed...
+};
