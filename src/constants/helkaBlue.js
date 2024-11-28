8db76eb506cb04aec94384fe026905fbb4277782
zakerous <m.z.humayoon@gmail.com>
2024-11-28 18:04:11 +0300

    deleted unnecessary stuff

--- src/constants/helkaBlue.js
+++
@@ -1,5 +0,0 @@
-export const helkaBlue = "#5699B1";
-export const helkaBrown = "#906E50";
-export const helkaOrange = "#D65F32";
-export const helkaDarkBrown = "#4E4F4E";
-export const helkaPastelGreen = "#929E8C";
