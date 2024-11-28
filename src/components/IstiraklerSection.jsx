cd74a36000b23169873cfae6094f3f8c7046710c
zakerous <m.z.humayoon@gmail.com>
2024-11-28 15:57:45 +0300

    IsitraklerSection: Done (Not responsive)

--- src/components/IstiraklerSection.jsx
+++ src/components/IstiraklerSection.jsx
@@ -5,34 +5,44 @@ import { istirakler } from "../constants";
 const IstiraklerSection = () => {
   return (
     <div className="istirakler-section">
-      <div className="white-bg bg-white rounded-[90px] min-h-72 px-24 mx-32 shadow-xl flex flex-col justify-center items-center relative">
-        <div className="title font-gunterz font-bold text-7xl text-helkaDarkBrown p-16">
-          <h1>İştirakerlimiz</h1>
-        </div>
-        <div className="background-grid">
+      <div className="white-bg bg-white rounded-[90px] min-h-72 px-24 mx-32 shadow-xl flex flex-col justify-center items-center mb-40">
+        <div className="background-grid relative pb-16">
+          <h1 className="title font-gunterz font-bold text-7xl text-helkaDarkBrown p-16 relative">
+            İştirakerlimiz
+          </h1>
           <img src={istiraklerSectionContainers} alt="" />
-        </div>
-        <div className="texts-images flex flex-col">
-          {/* a dynamic way to fetch texts 
-        the tile and the body must have different transition*/}
-          {istirakler.map((istirak, index) => (
-            <div
-              key={index}
-              className={`font-gunterz transition-all duration-300 ease-in-out flex gap-2   items-center mb-[30px] ${
-                index % 2 === 1 ? "flex-row-reverse" : "flex-row text-right"
-              }`}
-            >
-              <div className="texts w-1/2">
-                <h2 className={`text-${istirak.title_color}`}>
-                  {istirak.title}
-                </h2>
-                <p>{istirak.description}</p>
+          <div className="texts-images flex flex-col absolute top-[200px]">
+            {istirakler.map((istirak, index) => (
+              <div
+                key={index}
+                className={`font-gunterz transition-all duration-300 ease-in-out flex gap-2 items-center mb-[50px] ${
+                  index % 2 === 1 ? "flex-row-reverse" : "flex-row text-right"
+                }`}
+              >
+                <div className="texts w-1/2">
+                  <h2
+                    className={`text-${istirak.title_color} font-bold text-[28px]`}
+                  >
+                    {istirak.title}
+                  </h2>
+                  <p className="font-regular text-[10px]">
+                    {istirak.description}
+                  </p>
+                </div>
+                <div className="image w-1/2">
+                  <img
+                    src={istirak.image}
+                    alt={`${istirak.title} Image`}
+                    className={`max-w-[408px] ${
+                      index % 2 === 1
+                        ? "pr-[50px] pl-[12px] pb-[15px] pt-[19px]"
+                        : "pl-[55px] pr-[8px] pb-[10px] pt-[10px]"
+                    }  `}
+                  />
+                </div>
               </div>
-              <div className="image w-1/2">
-                <img src={istirak.image} alt={`${istirak.title} Image`} />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
