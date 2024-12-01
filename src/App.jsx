b234a9c5236c25f028d1fdb6131cd84ba19c2c36
zakerous <m.z.humayoon@gmail.com>
2024-12-01 18:09:16 +0300

    A simple transition between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import styles from "./styles";
 import { Footer, Navbar, Hero } from "./components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Kurumsal from "./pages/Kurumsal";
 import Projeler from "./pages/Projeler";
@@ -9,24 +14,37 @@ import Medya from "./pages/Medya";
 import DevamEdenProjeler from "./pages/DevamEdenProjeler";
 import TamamlananProjeler from "./pages/TamamlananProjeler";
 
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.pathname} classNames="fade" timeout={500}>
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/kurumsal" element={<Kurumsal />} />
+          <Route path="/projeler" element={<Projeler />} />
+          <Route path="/projeler/devam-eden" element={<DevamEdenProjeler />} />
+          <Route
+            path="/projeler/tamamlanan-eden"
+            element={<TamamlananProjeler />}
+          />
+          <Route path="/istiraklerimiz" element={<Istiraklerimiz />} />
+          <Route path="/medya" element={<Medya />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
 const App = () => (
   <div className="bg-helkaBgWhite flex flex-col w-full overflow-hidden">
     <div className={`${styles.paddingX}`}>
       <Navbar />
     </div>
-    <Routes>
-      {/* Other Routes */}
-      <Route path="/" element={<Home />} />
-      <Route path="/kurumsal" element={<Kurumsal />} />
-      <Route path="/projeler" element={<Projeler />} />
-      <Route path="/projeler/devam-eden" element={<DevamEdenProjeler />} />
-      <Route
-        path="/projeler/tamamlanan-eden"
-        element={<TamamlananProjeler />}
-      />
-      <Route path="/istiraklerimiz" element={<Istiraklerimiz />} />
-      <Route path="/medya" element={<Medya />} />
-    </Routes>
+    <AnimatedRoutes />
 
     <div className={`bg-footerBg ${styles.flexCenter}`}>
       <Footer />
